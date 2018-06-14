893ddb1a3542c290d6915067f5c8e5f807469ac9
Jibin M <jmathew1991@gmail.com>
2018-06-14 15:53:32 -0400

    testing things out

--- server/app.js
+++ server/app.js
@@ -22,24 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(process.env.PORT || 8081, function(){
+  console.log('Server is started on port 8081');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(3000, function(){
-  console.log('Server is started on port 3000');
+app.get('/status', (req,res) =>{
+  console.log('gottem');
+  res.end();
 });
 
 module.exports = app;
