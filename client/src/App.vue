bc6e57602ab6b25e1c2c52badf0360edb150190f
Jibin M <jmathew1991@gmail.com>
2018-06-14 15:19:56 -0400

    First Commit

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <div id="app">
+          <MainNavigation/>
+          <div id="nav">
+asdfasdfs
+          </div>
+          <router-view/>
+        </div>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+
+<script>
+import MainNavigation from '@/components/MainNavigation.vue';
+
+export default {
+  components: {
+    MainNavigation,
+  },
+};
+</script>
---
+++ client/src/components/MainNavigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>Product Bundler</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat to="/">Home</v-btn>
+      <v-btn flat to="/about">About</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'MainNavigation',
+  props: {
+    msg: String,
+  },
+};
+</script>
