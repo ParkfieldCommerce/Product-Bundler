c47af6d690fcc8a00d1c940ffeac8233791c7cd5
Jibin M <jmathew1991@gmail.com>
2018-07-22 19:30:16 -0400

    Added Catches for Async functions

--- server/scripts/shopify-functions.js
+++ server/scripts/shopify-functions.js
@@ -14,9 +14,13 @@ const shopify = new Shopify({
 /* Set of Functions To Retrieve All the Products From the Store */
 async function getAllProducts(){
   //Returns all the products in the store to send to client server
-  let numOfProducts = await getNumOfProducts();
-  let productsList = await fetchProducts(Math.ceil(numOfProducts/250));
-  return productsList;
+  try{
+    let numOfProducts = await getNumOfProducts();
+    let productsList = await fetchProducts(Math.ceil(numOfProducts/250));
+    return productsList;
+  }catch(e){
+    console.log('Error Fetching All Products');
+  }
 }
 function getNumOfProducts(){
   //Gets the number of products in the store
@@ -24,55 +28,75 @@ function getNumOfProducts(){
 }
 async function fetchProducts(page, productList = []){
   //Returns all the product objects in the store
-  if(page != 0){
-    let products = await getPageOfProducts(page);
-    let updatedProductList = [...productList, ...products];
-    return await fetchProducts(page - 1, updatedProductList);
-  }else{
-    return productList;
+  try{
+    if(page != 0){
+      let products = await getPageOfProducts(page);
+      let updatedProductList = [...productList, ...products];
+      return await fetchProducts(page - 1, updatedProductList);
+    }else{
+      return productList;
+    }
+  }catch(e){
+    console.log('Error fetching producs');
   }
 }
 async function getPageOfProducts(page){
   //Gets the total number of pages with 250 products
-  let products = await shopify.product.list({ limit:250, page });
-  return products;
+  try{
+    let products = await shopify.product.list({ limit:250, page });
+    return products;
+  }catch(e){
+    console.log('Error getting page of products');
+  }
 }
 
 /* Set of Functions to Create and Return a Bundled Box */
 async function createBox(metafields, items, boxDescription){
   //Starts process to create the box
-  let box = await createProduct({
-    title: BOXNAME,
-    product_type: "Built Box",
-    metafields,
-    variants:[{
-      "price":items.price
-    }]
-  });
-  box.boxDescription = boxDescription;
-  await enableSalesChannel(box);
-  return box;
+  try{
+    let box = await createProduct({
+      title: BOXNAME,
+      product_type: "Built Box",
+      metafields,
+      variants:[{
+        "price":items.price
+      }]
+    });
+    box.boxDescription = boxDescription;
+    await enableSalesChannel(box);
+    return box;
+  }catch(e){
+    console.log('Error creating box');
+  }
 }
 async function createProduct(productDetails){
   //Creates the box on Shopify and returns the object
-  let product = await shopify.product.create(productDetails)
-  .then( response => {
-    return response;
-  })
-  .catch((err)=> {
-    console.log(err);
-  });
-  return product;
+  try{
+    let product = await shopify.product.create(productDetails)
+    .then( response => {
+      return response;
+    })
+    .catch((err)=> {
+      console.log(err);
+    });
+    return product;
+  }catch(e){
+    console.log('Error creating a product');
+  }
 }
 async function enableSalesChannel(product){
   //Enable product on sales channel to checkout
-  await shopify.productListing.create(product.id)
-  .then((response) => {
-    console.log('Product Successfully Enabled on App Sales Channel');
-  })
-  .catch((err) =>{
-    console.log(err);
-  });
+  try{
+    await shopify.productListing.create(product.id)
+    .then((response) => {
+      console.log('Product Successfully Enabled on App Sales Channel');
+    })
+    .catch((err) =>{
+      console.log(err);
+    });
+  }catch(e){
+    console.log('Error enabling sales channel');
+  }
 }
 
 /* Set of Functions to handle a completed order with a box */
@@ -88,20 +112,24 @@ async function handleOrderCreation(orderItems){
 }
 async function getMetafields(productId){
   //Returns all the metafields for the box
-  let metafields = [];
-  await shopify.metafield.list({
-    metafield:{
-      owner_resource:'product',
-      owner_id: productId
-    }
-  })
-  .then((res) => {
-    metafields = res;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-  return metafields;
+  try{
+    let metafields = [];
+    await shopify.metafield.list({
+      metafield:{
+        owner_resource:'product',
+        owner_id: productId
+      }
+    })
+    .then((res) => {
+      metafields = res;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    return metafields;
+  }catch(e){
+    console.log('Error getting metafields');
+  }
 }
 function getProductsToUpdate(metafields){
   //Finds which items need inventory updated
@@ -123,21 +151,25 @@ function getProductsToUpdate(metafields){
 }
 async function updateProductQuantity(inventoryList){
   //Adjusts the quantity for each item in the box
-  let locations = await shopify.location.list();
-  let locationId = locations[0].id;
-  inventoryList.forEach( item => {
-    shopify.inventoryLevel.adjust({
-      location_id: locationId,
-      inventory_item_id: item.id, 
-      available_adjustment: item.quantity*-1
-    })
-    .then( success => {
-      console.log(`${success.inventory_item_id} inventory updated`);
-    })
-    .catch( err => {
-      console.log(err);
+  try{
+    let locations = await shopify.location.list();
+    let locationId = locations[0].id;
+    inventoryList.forEach( item => {
+      shopify.inventoryLevel.adjust({
+        location_id: locationId,
+        inventory_item_id: item.id, 
+        available_adjustment: item.quantity*-1
+      })
+      .then( success => {
+        console.log(`${success.inventory_item_id} inventory updated`);
+      })
+      .catch( err => {
+        console.log(err);
+      });
     });
-  });
+  }catch(e){
+    console.log('Error updating product quantity');
+  }
 }
 function deleteBoxProduct(productId){
   //Deletes the box from the Shopify Store
